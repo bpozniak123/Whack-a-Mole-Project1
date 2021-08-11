79ed94e812d23b832cc39f3b4c1bab2683df33c0
bpozniak <benjaminpozniak@gmail.com>
2021-08-10 21:13:00 -0400

    Initial Setup Complete- JS class, html key game components, basic css format

---
+++ js/game.js
@@ -0,0 +1,35 @@
+console.log("Whack-a-Mole")
+
+//Build blue print using a class - Construction of the "board"
+class Whack{
+	constructor(name){
+		this.name = name;
+		this.timer = 30;
+		this.score = 0;
+		// this.highScore = [];
+		this.moles = [];
+		this.holes = [];
+	}
+
+}
+
+
+//Use methods to create variables
+
+
+// Create the game 
+
+const gameSetup = {
+
+
+
+	startGame(){
+		
+	}
+
+}
+
+//functions for game play
+
+
+//End game

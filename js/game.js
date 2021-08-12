76da4fee92be439cf261ae03de5e181b0be5dede
bpozniak <benjaminpozniak@gmail.com>
2021-08-11 20:04:47 -0400

    random time func added and holes func complete

--- js/game.js
+++ js/game.js
@@ -1,35 +1,69 @@
 console.log("Whack-a-Mole")
 
-//Build blue print using a class - Construction of the "board"
-class Whack{
-	constructor(name){
-		this.name = name;
-		this.timer = 30;
-		this.score = 0;
-		// this.highScore = [];
-		this.moles = [];
-		this.holes = [];
-	}
+//Game Selectors
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const score = document.querySelector('#Score');
 
-}
+
+//Build blue print using a class - Construction of the "board"
+// class Whack{
+// 	constructor(name){
+// 		this.name = name;
+// 		this.timer = 30;
+// 		this.gameOver = false;
+// 		this.score = 0;
+// 		this.round = 0;
+// 		// this.highScore = [];
+// 		this.moles = [];
+// 		this.holes = [];
+// 	}
+
+// 	end() {
+// 		let timeInterval = setInterval(()=>{
+// 			this.gameGoing()
+// 			if (this.gameOver === true) {
+// 				clearInterval(timeInterval)
+// 			} 
+
+// 		})
+// 	}
+// }
 
 
 //Use methods to create variables
 
-
-// Create the game 
-
-const gameSetup = {
-
-
-
-	startGame(){
-		
-	}
+//Create a function setting up mole to pop up randomly
+function randomTime(min, max){
+	return Math.random() * (max - min) + min;
 
 }
 
-//functions for game play
+//function for array of holes. Need to eventually incorporate more logic to avoid mole pop up to same hole.
+function randomHoles(holes){ 
+	const index = Math.floor(Math.random() * hole.length);
+	const hole = holes[index];
+}
+console.log(holes.length
+	)
+//Create a function for mole up
+function moleMoves(){
+	const time = 
+	const hole = 
+}
+// Create the game 
+
+const gameSetup = {
+	//scoreboard
+	//score
+	//gameOver
+
+}
+
+function whack(){
+	//click
+	//score++
+}
 
 
 //End game
